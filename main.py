04f9b6bf036a3882c8a12c8c721e3dd4df3e86e6
Thilakpriyan <rameshthilakpriyan@gmail.com>
2026-06-24 23:34:44 +0530

    Added HSV Blue Colour Detection

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    success, frame = cap.read()
+    success, img = cap.read()
 
-    if not success:
-        print("Failed to access webcam")
-        break
+    # Convert BGR → HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow("Virtual Paint - Camera Test", frame)
+    # Blue color range
+    lower_blue = np.array([90, 100, 50])
+    upper_blue = np.array([140, 255, 255])
+
+    # Create mask
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    cv2.imshow("Camera", img)
+    cv2.imshow("Mask", mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
